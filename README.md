609e714921eff80d2285da29e8d1caaab9822b00
LatekVon <latkaignacy@gmail.com>
2021-04-10 00:12:34 +0200

    implemented saved-ai parsing base, learned about some compilers, ruined my sleep schedule

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # evolution-algo-1
 All things written here may be work in progress, as such, may not be **yet** included in the code.
 
-Improved version of my old evolution-algo-0.
-Dubbed face-fronter, but able of manipulation of any vector of numbers (32-bit float).
-Unlike old algorithm, this one was designed with modularity in mind from start, as such,
-it divides every structure as parent of multiple children with vectors.
-Added "super-nodes" which store multiple other nodes, which makes things more efficient as long as we dont count unused nodes inside,
-as this enables each node to "choose" type of equasion from those that happen to be inside "supernode" and enable more
-complicated equsaions without really adding layers. Now inputs dont effect all nodes, but only affect fraction of nodes that are "input nodes",
-this will help spot and eliminate unused nodes and skip them.
+Improved version of my old evolution-algo-0. <br>
+Dubbed face-fronter, but able of manipulation of any vector of numbers (32-bit float). <br>
+Unlike old algorithm, this one was designed with modularity in mind from start, as such, <br>
+it divides every structure as parent of multiple children with vectors. <br>
+Added "super-nodes" which store multiple other nodes, which makes things more efficient as long as we dont count unused nodes inside, <br>
+as this enables each node to "choose" type of equasion from those that happen to be inside "supernode" and enable more <br>
+complicated equsaions without really adding layers. Now inputs dont effect all nodes, but only affect fraction of nodes that are "input nodes", <br>
+this will help spot and eliminate unused nodes and skip them. <br>
--- src/ai_rw.rs
+++ src/ai_rw.rs
@@ -1,3 +1,8 @@
+//use std::io;
+//use std::io::prelude::*;
+//use std::fs::File;
+//use std::fs;
+
 use super::ai_module as ai_module;
 
 /* General save idea:
@@ -43,7 +48,71 @@ pub fn save(ai: &ai_module::Ai) {
      
 
 }
-pub fn load() -> ai_module::Ai {
+pub fn load(name: &str) -> Option<ai_module::Ai> {
+ 
+    //first few characters are already parsed from start
+    
+    //metadata
+    let n: String = String::new();
+    let l: String = String::new();
+    let gen: String = String::new();
+    
+    let ai_s: String;
+    let mut ai_v: Vec<&str> = Vec::new();
+    if std::path::Path::new(name).exists() {
+        
+        ai_s = String::from(std::fs::read_to_string(name).unwrap().trim());
+        //initial metadata loop   
+        
+        let n_b = true;
+        let l_b = true;
+        let _gen_b = true;
+
+        loop {
+            let c = ai_s.chars().nth(0).expect("ai_rw -> Opened file is empty");
+            ai_s.remove(0);
+
+            if n_b {
+                if c == ';' {
+                    n_b = false;
+                    continue;
+                } else { 
+                    n.push(c);
+                }
+            }
+            if l_b {
+                if c == ';' {
+                    l_b = false;
+                    continue;
+                } else { 
+                    n.push(c);
+                }
+            }
+
+            break;
+
+        }
 
 
+    } else {
+        //return quiet error
+        return None; 
+    }
+
+    //metadata: &str to usize 
+    let n: usize = n.parse().unwrap();
+    let l: usize = l.parse().unwrap();
+    let gen: usize = gen.parse().unwrap();
+
+    ai_v.push(&ai_s);
+    ai_v.split(|c| c.chars().next().unwrap() == ';');
+    //Parser part
+    let ai_o = ai_module::Ai::new(n, l);
+   
+    println!("{:?}", ai_v); //DEBUG
+
+    //gonna add this later
+    //ai_o.set_gen(gen);
+
+    Some(ai_o)
 }
